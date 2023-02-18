173ed43221de09e8fa95a5c8ad649514c8043085
deneb1593 <deneb1593@gmail.com>
2023-02-18 17:22:38 +0900

    TextBlock 추가

---
+++ README.md
@@ -0,0 +1,13 @@
+### JDK 17
+
+### JDK 17의 새로운 기능 및 변경점을 확인한다.
+
+#### [개발환경]
+* JDK 17
+ 
+#### [JDK 17 새로운 기능]
+* text block
+
+
+#### [참조]
+* [JEP 355: Text Blocks](https://openjdk.org/jeps/355)
---
+++ src/test/java/TextBlockTester.java
@@ -0,0 +1,60 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TextBlockTester {
+
+  @Test
+  void SQL_Test() {
+    Long id = 10L;
+    String name = "kim";
+
+    Object[] params = new Object[] { id, name };
+
+    String sql = """
+      SELECT
+          ID,
+          NAME,
+          CREATED_AT
+        FROM POST
+        WHERE
+            ID = %s
+        AND NAME = %s
+      """
+      .formatted(params);
+
+    assertThat(sql).contains(String.valueOf(id));
+    assertThat(sql).contains(name);
+  }
+
+
+  @Test
+  void HTML_test() {
+    String name = "DENEB";
+
+    String template =
+      """
+      <html>
+        <head>
+          <title>Hello</title>
+        </head>
+        <body>
+          <h2>Welcome to my site</h2>
+          <p id="name">%s</p> 
+        </body>
+      </html>
+      """
+        .formatted(name);
+
+    Document document = Jsoup.parse(template);
+
+    String val = document.getElementById("name").text();
+
+    assertThat(val).isEqualTo(name);
+  }
+
+
+}
